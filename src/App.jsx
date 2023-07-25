b5243f214d8ac61411abf24e9b8d6f65b6f92135
Woranan Hernandez <worananhernandez@Woranans-Air.lan>
2023-07-25 10:53:12 -0700

    updated

--- src/App.jsx
+++ src/App.jsx
@@ -1,34 +1,29 @@
-import React from 'react'
+import { useState} from 'react'
+//useState to refllect the updated data.
+import {puppyList} from './data.js'
+
 //import reactLogo from './assets/react.svg'
 //import viteLogo from '/vite.svg'
 import './App.css'
 
 function App() {
-  //const [count, setCount] = useState(0)
+  //invoke the hook, usestate return an array an arr contains two element
+  // first is the value that storing
+  // second is a function which you can use to reset the value
+  const [puppies, setPuppies] = useState(puppyList)
+   console.log("puppyList: ", puppyList);
+
+  //console.log(puppies, setPuppies)
 
   return (
-    <>
-      {/* <div>
-        <a href="https://vitejs.dev" target="_blank">
-          <img src={viteLogo} className="logo" alt="Vite logo" />
-        </a>
-        <a href="https://react.dev" target="_blank">
-          <img src={reactLogo} className="logo react" alt="React logo" />
-        </a>
-      </div>
-      <h1>Vite + React</h1>
-      <div className="card">
-        <button onClick={() => setCount((count) => count + 1)}>
-          count is {count}
-        </button>
-        <p>
-          Edit <code>src/App.jsx</code> and save to test HMR
-        </p>
-      </div>
-      <p className="read-the-docs">
-        Click on the Vite and React logos to learn more
-      </p> */}
-    </>
+    <div className="App">
+      {
+        puppies.map((puppy)=> {
+          return <p key= {puppy.id}>{puppy.name}</p>
+        })
+      }
+      
+    </div>
   )
 }
 
